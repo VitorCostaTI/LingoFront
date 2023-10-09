fcab2f53688fd669759e035cb37aa9609f001b5e
Vitor Costa <vitortecnologia62@gmail.com>
2023-10-09 12:12:30 -0300

    Dialog Produtos

--- src/app/components/pages/produtos/produtos.component.ts
+++ src/app/components/pages/produtos/produtos.component.ts
@@ -11,16 +11,16 @@ export interface DespesaElement {
 }
 
 const ELEMENT_DATA: DespesaElement[] = [
-  {produto: 'Iphone', fornecedor: 'Apple', preco: 'R$ 6.000,00', investimento: 'R$ 3.000,00', qtd: '82'},
-  {produto: 'Iphone', fornecedor: 'Apple', preco: 'R$ 6.000,00', investimento: 'R$ 3.000,00', qtd: '82'},
-  {produto: 'Iphone', fornecedor: 'Apple', preco: 'R$ 6.000,00', investimento: 'R$ 3.000,00', qtd: '82'},
-  {produto: 'Iphone', fornecedor: 'Apple', preco: 'R$ 6.000,00', investimento: 'R$ 3.000,00', qtd: '82'},
-  {produto: 'Iphone', fornecedor: 'Apple', preco: 'R$ 6.000,00', investimento: 'R$ 3.000,00', qtd: '82'},
-  {produto: 'Iphone', fornecedor: 'Apple', preco: 'R$ 6.000,00', investimento: 'R$ 3.000,00', qtd: '82'},
-  {produto: 'Iphone', fornecedor: 'Apple', preco: 'R$ 6.000,00', investimento: 'R$ 3.000,00', qtd: '82'},
-  {produto: 'Iphone', fornecedor: 'Apple', preco: 'R$ 6.000,00', investimento: 'R$ 3.000,00', qtd: '82'},
-  {produto: 'Iphone', fornecedor: 'Apple', preco: 'R$ 6.000,00', investimento: 'R$ 3.000,00', qtd: '82'},
-  {produto: 'Iphone', fornecedor: 'Apple', preco: 'R$ 6.000,00', investimento: 'R$ 3.000,00', qtd: '82'},
+  {produto: 'Shampoo', fornecedor: 'Meau', preco: 'R$ 7,40', investimento: 'R$ 4,00', qtd: '92'},
+  {produto: 'Condicionador', fornecedor: 'Meau', preco: 'R$ 12,00', investimento: 'R$ 6,00', qtd: '75'},
+  {produto: 'Perfume Dog Macho', fornecedor: 'Pet Clean', preco: 'R$ 16,00', investimento: 'R$ 9,00', qtd: '30'},
+  {produto: 'Perfume Dog Femea', fornecedor: 'Pet Clean', preco: 'R$ 18,00', investimento: 'R$ 9,00', qtd: '25'},
+  {produto: 'Coleira', fornecedor: 'Mesh Reserva', preco: 'R$ 149,90', investimento: 'R$ 74,00', qtd: '12'},
+  {produto: 'Ração', fornecedor: 'Special Dog', preco: 'R$ 249,90', investimento: 'R$ 74,00', qtd: '18'},
+  {produto: 'Brinquedo Osso', fornecedor: 'Genérico', preco: 'R$ 1,90', investimento: 'R$ 0,50', qtd: '9'},
+  {produto: 'Rastreador Ocultaneo', fornecedor: 'Genérico', preco: 'R$ 1,90', investimento: 'R$ 0,80', qtd: '13'},
+  {produto: 'V10', fornecedor: 'Nobivac', preco: 'R$ 249,00', investimento: 'R$ 149,00', qtd: '16'},
+  {produto: 'Vermifugo', fornecedor: 'Milbemax', preco: 'R$ 249,00', investimento: 'R$ 149,00', qtd: '2'},
   
 ];
 
