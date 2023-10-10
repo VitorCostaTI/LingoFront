75c857945c3d0932d224b07cb203bfbd7a385e34
Vitor Costa <vitortecnologia62@gmail.com>
2023-10-10 11:14:55 -0300

    atualiza listagem de clientes no dashboard

--- src/app/components/pages/dashboard/dashboard.component.ts
+++ src/app/components/pages/dashboard/dashboard.component.ts
@@ -14,10 +14,10 @@ export interface DespesaElement {
 }
 
 const ELEMENT_DATA: DespesaElement[] = [
-  {data: new Date('2021-04-19T10:00:00.000'), cliente: 'Catarina Pietra Elaine Carvalho', pet: 'Tobel', servico: 'Tosa', telefone: '(88) 2726-1456'},
-  {data: new  Date('2021-04-21T10:00:00.000'), cliente: 'Daniel Pedro Henrique Emanuel Nogueira', pet: 'Taone', servico: 'Banho', telefone: '(14) 2915-2920'},
-  {data: new Date('2021-04-22T10:00:00.000'), cliente: 'Carla Sophia Nogueira', pet: 'Cyepe', servico: 'Passeio', telefone: '(84) 2630-6718'},
-  {data: new Date('2021-04-23T10:00:00.000'), cliente: 'Bárbara Isabel Lopes', pet: 'Launo', servico: 'Hospedagem', telefone: '(82) 2375-2185'},
+  {data: new Date('2021-04-19T10:00:00.000'), cliente: 'Marcela Maya Luiza da Paz', pet: 'Tobel', servico: 'Tosa', telefone: '(88) 2726-1456'},
+  {data: new  Date('2021-04-21T10:00:00.000'), cliente: 'Marlene Nicole Eduarda Silva', pet: 'Taone', servico: 'Banho', telefone: '(14) 2915-2920'},
+  {data: new Date('2021-04-22T10:00:00.000'), cliente: 'Cauê Daniel Carlos Moreira', pet: 'Cyepe', servico: 'Passeio', telefone: '(84) 2630-6718'},
+  {data: new Date('2021-04-23T10:00:00.000'), cliente: 'Gabriel Julio Geraldo Ramos', pet: 'Launo', servico: 'Hospedagem', telefone: '(82) 2375-2185'},
 ];
 
 @Component({
