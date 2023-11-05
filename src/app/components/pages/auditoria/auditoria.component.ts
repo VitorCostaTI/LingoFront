662918b66f8aab03170b015a9a7d074d197666ec
Vitor Costa <vitortecnologia62@gmail.com>
2023-11-05 18:02:34 -0300

    Tabela Auditoria

--- src/app/components/pages/auditoria/auditoria.component.ts
+++ src/app/components/pages/auditoria/auditoria.component.ts
@@ -5,20 +5,20 @@ export interface AuditoriaElement {
   setor: string;
   supervisor: string;
   atividade: string;
-  hora: string;
+  hora: Date;
 }
 
 const ELEMENT_DATA: AuditoriaElement[] = [
-  {colaborador: 'Cláudio Raul Lima', setor: 'Software', supervisor: 'Kaique Fernando Rocha', atividade: '', hora: ''},
-  {colaborador: 'Tatiane Larissa Emily Aragão', setor: 'Administrativo', supervisor: 'Nair Antônia Sales', atividade: '', hora: ''},
-  {colaborador: 'Giovanna Marli da Cunha', setor: 'Financeiro', supervisor: 'Sérgio Pietro Ruan Fogaça', atividade: '', hora: ''},
-  {colaborador: 'Cláudio Raul Lima', setor: 'Software', supervisor: 'Kaique Fernando Rocha', atividade: '', hora: ''},
-  {colaborador: 'Giovanna Marli da Cunha', setor: 'Financeiro', supervisor: 'Sérgio Pietro Ruan Fogaça', atividade: '', hora: ''},
-  {colaborador: 'Tatiane Larissa Emily Aragão', setor: 'Administrativo', supervisor: 'Nair Antônia Sales', atividade: '', hora: ''},
-  {colaborador: 'Cláudio Raul Lima', setor: 'Software', supervisor: 'Kaique Fernando Rocha', atividade: '', hora: ''},
-  {colaborador: 'Tatiane Larissa Emily Aragão', setor: 'Administrativo', supervisor: 'Nair Antônia Sales', atividade: '', hora: ''},
-  {colaborador: 'Giovanna Marli da Cunha', setor: 'Financeiro', supervisor: 'Sérgio Pietro Ruan Fogaça', atividade: '', hora: ''},
-  {colaborador: 'Cláudio Raul Lima', setor: 'Software', supervisor: 'Kaique Fernando Rocha', atividade: '', hora: ''},
+  {colaborador: 'Cláudio Raul Lima', setor: 'Software', supervisor: 'Kaique Fernando Rocha', atividade: 'Cadastro Produto', hora: new Date('2021-04-19T10:00:00.000')},
+  {colaborador: 'Tatiane Larissa Emily Aragão', setor: 'Administrativo', supervisor: 'Nair Antônia Sales', atividade: 'Atualiza OS', hora: new Date('2021-04-19T10:00:00.000')},
+  {colaborador: 'Giovanna Marli da Cunha', setor: 'Financeiro', supervisor: 'Sérgio Pietro Ruan Fogaça', atividade: 'Cadastro Cliente', hora: new Date('2021-04-19T10:00:00.000')},
+  {colaborador: 'Cláudio Raul Lima', setor: 'Software', supervisor: 'Kaique Fernando Rocha', atividade: 'Cadastro Colaborador', hora: new Date('2021-04-19T10:00:00.000')},
+  {colaborador: 'Giovanna Marli da Cunha', setor: 'Financeiro', supervisor: 'Sérgio Pietro Ruan Fogaça', atividade: 'Cadastro Produto', hora: new Date('2021-04-19T10:00:00.000')},
+  {colaborador: 'Tatiane Larissa Emily Aragão', setor: 'Administrativo', supervisor: 'Nair Antônia Sales', atividade: 'Atualiza OS', hora: new Date('2021-04-19T10:00:00.000')},
+  {colaborador: 'Cláudio Raul Lima', setor: 'Software', supervisor: 'Kaique Fernando Rocha', atividade: 'Cadastro Cliente', hora: new Date('2021-04-19T10:00:00.000')},
+  {colaborador: 'Tatiane Larissa Emily Aragão', setor: 'Administrativo', supervisor: 'Nair Antônia Sales', atividade: 'Cadastro Colaborador', hora: new Date('2021-04-19T10:00:00.000')},
+  {colaborador: 'Giovanna Marli da Cunha', setor: 'Financeiro', supervisor: 'Sérgio Pietro Ruan Fogaça', atividade: 'Atualiza OS', hora: new Date('2021-04-19T10:00:00.000')},
+  {colaborador: 'Cláudio Raul Lima', setor: 'Software', supervisor: 'Kaique Fernando Rocha', atividade: 'Cadastro Colaborador', hora: new Date('2021-04-19T10:00:00.000')},
 ];
 
 @Component({
