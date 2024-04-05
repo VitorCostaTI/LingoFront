9184c123d0841249eb2839429ce472e866e0cf29
VitorCostaTI <vitortecnologia62@gmail.com>
2024-04-05 13:21:00 -0300

    Permissão de acesso: funcionalidade de permissão

--- src/app/components/pages/permissao-acesso/permissao-acesso.component.ts
+++ src/app/components/pages/permissao-acesso/permissao-acesso.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { PermissaoAcessoDialogComponent } from './permissao-acesso-dialog/permissao-acesso-dialog.component';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-permissao-acesso',
@@ -9,7 +10,10 @@ import { PermissaoAcessoDialogComponent } from './permissao-acesso-dialog/permis
 })
 
 export class PermissaoAcessoComponent {
-  constructor(public dialog: MatDialog) {}
+  constructor(public dialog: MatDialog, private _snackBar: MatSnackBar) { }
+
+  horizontalPosition: MatSnackBarHorizontalPosition = 'start';
+  verticalPosition: MatSnackBarVerticalPosition = 'bottom';
 
   typesOfShoes: string[] = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
 
@@ -18,6 +22,14 @@ export class PermissaoAcessoComponent {
       width: '750px',
       enterAnimationDuration,
       exitAnimationDuration,
+    });
+  }
+
+  autorizarSetor(): void {
+    this._snackBar.open("Permissão salva com sucesso", "Fechar", {
+      duration: 1500,
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
     });
   }
 }
