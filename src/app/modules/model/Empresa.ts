089fdcab8f1300d3e01dd93b34f87e3620b3934d
VitorCostaTI <vitortecnologia62@gmail.com>
2024-07-26 14:07:41 -0300

    CodeReview

--- src/app/modules/model/Empresa.ts
+++ src/app/modules/model/Empresa.ts
@@ -1,7 +1,7 @@
 export interface Empresa {
-    razao_social: string;
-    nome_fantasia: string;
-    cnpj: string;
-    representante: string;
-    contato: string;
-  }
+  razao_social: string;
+  nome_fantasia: string;
+  cnpj: string;
+  representante: string;
+  contato: string;
+}
