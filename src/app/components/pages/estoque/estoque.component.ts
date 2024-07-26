0f5e5f590ee4af9fecca0e67583ed9f2b0ef5b11
VitorCostaTI <vitortecnologia62@gmail.com>
2024-07-26 14:25:09 -0300

    Colaborador_DATA

--- src/app/components/pages/estoque/estoque.component.ts
+++ src/app/components/pages/estoque/estoque.component.ts
@@ -1,16 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { TemplateCrudService } from 'src/app/modules/services/Template/template-crud.service';
-
-export interface ColaboradoresElement {
-  colaborador: string;
-  setor: string;
-  localizacao: string;
-  email: string;
-  telefone: string;
-  telefone2: string;
-  emergencia: string;
-}
+import { Colaborador_DATA } from 'src/Database/Colaborador';
 
 export interface EstoqueElement {
   codigo: string;
@@ -72,106 +63,22 @@ const ELEMENT_DATA: EstoqueElement[] = [
 
 export class EstoqueComponent {
 
-  colaborador: ColaboradoresElement[] = [
-    {
-      colaborador: 'José Vicente Jesus',
-      setor: 'Contabilidade',
-      localizacao: 'Anápolis City, Anápolis - GO',
-      email: 'jose_vicente_jesus@leandroreis.com',
-      telefone: '(62) 3854-9878',
-      telefone2: '(62) 99276-0741',
-      emergencia: 'Pai - (62) 98276-0747'
-    },
-    {
-      colaborador: 'Fernanda Marlene Novaes',
-      setor: 'Operacional',
-      localizacao: 'Jardim Novo Horizonte, Cuiabá - MT',
-      email: 'fernanda-novaes98@msds.com.br',
-      telefone: '(65) 2854-8027',
-      telefone2: '(65) 98105-9206',
-      emergencia: 'Mãe - (65) 99976-0741'
-    },
-    {
-      colaborador: 'Nair Antônia Sales',
-      setor: 'Administrativo',
-      localizacao: 'Jardim São José (Zona Norte), São Paulo - SP',
-      email: 'nair-sales81@lnaa.com.br',
-      telefone: '(11) 2527-8445',
-      telefone2: '(11) 99861-5754',
-      emergencia: 'Marido - (11) 99576-0741'
-    },
-    {
-      colaborador: 'Sérgio Pietro Ruan Fogaça',
-      setor: 'Financeiro',
-      localizacao: 'Santo Antônio, Teresina - PI',
-      email: 'sergio-fogaca84@demasi.com.br',
-      telefone: '(86) 2675-0807',
-      telefone2: '(86) 99678-1010',
-      emergencia: 'Filho - (86) 92276-0741'
-    },
-    {
-      colaborador: 'Kaique Fernando Rocha',
-      setor: 'Software',
-      localizacao: 'Bom Sucesso, Imperatriz - MA',
-      email: 'kaique.fernando.rocha@vmetaiscba.com.br',
-      telefone: '(98) 2983-3202',
-      telefone2: '(98) 99136-3891',
-      emergencia: 'Esposa - (98) 99270-0741'
-    },
-    {
-      colaborador: 'Marcela Emilly Rayssa Gomes',
-      setor: 'Operacional',
-      localizacao: 'Alto da Balança, Fortaleza - CE',
-      email: 'marcelaemillygomes@regler.com.br',
-      telefone: '(85) 2646-7783',
-      telefone2: '(85) 99669-4503',
-      emergencia: 'Neto - (85) 99546-0741'
-    },
-    {
-      colaborador: 'Giovanna Marli da Cunha',
-      setor: 'Financeiro',
-      localizacao: 'Espinheiro, Recife - PE',
-      email: 'giovanna-dacunha70@ddfnet.com.br',
-      telefone: '(81) 2917-4236',
-      telefone2: '(81) 98492-3328',
-      emergencia: 'Primo - (81) 99276-2341'
-    },
-    {
-      colaborador: 'Tatiane Larissa Emily Aragão',
-      setor: 'Administrativo',
-      localizacao: 'Bairro São Domingos, Ponta Porã - MS',
-      email: 'tatiane_aragao@jmmarcenaria.com.br',
-      telefone: '(67) 3772-4865',
-      telefone2: '(67) 99595-1422',
-      emergencia: 'Tio - (67) 99286-0741'
-    },
-    {
-      colaborador: 'Ian Heitor Renan Fernandes',
-      setor: 'Operacional',
-      localizacao: 'Distrito Industrial, São José do Rio Preto - SP',
-      email: 'ian_heitor_fernandes@unitower.com.br',
-      telefone: '(17) 3793-0486',
-      telefone2: '(17) 98742-7498',
-      emergencia: 'Pai - (17) 99276-0711'
-    },
-    {
-      colaborador: 'Cláudio Raul Lima',
-      setor: 'Software',
-      localizacao: 'Condomínio Flor do Cerrado, Cuiabá - MT',
-      email: 'claudio-lima79@orthoi.com.br',
-      telefone: '(65) 2626-1380',
-      telefone2: '(65) 99274-9153',
-      emergencia: 'Pai - (65) 99271-0741'
-    },
-  ];
-
   registroEstoque: FormGroup;
+  colaborador: any[] = [];
 
   constructor(private fb: FormBuilder, private templateService: TemplateCrudService) {
     this.registroEstoque = this.fb.group({
       codigo_produto: ['', Validators.required],
       responsavel: ['', Validators.required],
     })
+  }
+
+  ngOnInit(): void {
+    this.getColaborador();
+  }
+
+  getColaborador(): void {
+    this.colaborador = Colaborador_DATA;
   }
 
   displayedColumns: string[] = [
@@ -185,7 +92,6 @@ export class EstoqueComponent {
 
   registrarEstoque(): void {
     this.templateService.snackBarSuccess("Produto registrado com sucesso!!", "")
-
     this.registroEstoque.reset();
   }
 
---
+++ src/app/modules/model/Colaborador.ts
@@ -0,0 +1,9 @@
+export interface Colaborador {
+    colaborador: string;
+    setor: string;
+    localizacao: string;
+    email: string;
+    telefone: string;
+    telefone2: string;
+    emergencia: string;
+}
