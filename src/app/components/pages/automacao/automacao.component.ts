cb471a093c81a7f5484bbff857b18998facd033c
VitorCostaTI <vitortecnologia62@gmail.com>
2024-07-26 14:36:57 -0300

    Automacao_DATA

--- src/app/components/pages/automacao/automacao.component.ts
+++ src/app/components/pages/automacao/automacao.component.ts
@@ -3,131 +3,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { ConfirmacaoComponent } from '../../dialogs/confirmacao/confirmacao.component';
 import { ConfirmacaoService } from 'src/app/modules/services/Confirmacao/confirmacao.service';
 import { TemplateCrudService } from 'src/app/modules/services/Template/template-crud.service';
-
-export interface MoradorElement {
-  status: string;
-  ip: string;
-  firmware: string;
-  modelo: string;
-  fabricante: string;
-  equipamento: string;
-  local: string;
-  porta: string;
-  ultima_manutencao: string;
-}
-
-const ELEMENT_DATA: MoradorElement[] = [
-  {
-    status: "Conectado",
-    ip: "192.168.0.192",
-    firmware: "V6.14.10",
-    modelo: "ControlID - IDFace",
-    fabricante: "ControlID",
-    equipamento: "Leitor Facial",
-    local: "Acesso Estoque",
-    porta: "4092",
-    ultima_manutencao: "2024-02-23T14:21:42.000-03:00"
-  },
-  {
-    status: "Conectado",
-    ip: "192.168.0.130",
-    firmware: "V6.14.10",
-    modelo: "ControlID - IDFace",
-    fabricante: "ControlID",
-    equipamento: "Leitor Facial",
-    local: "Acesso Canil",
-    porta: "4093",
-    ultima_manutencao: "2024-02-23T14:21:42.000-03:00"
-  },
-  {
-    status: "Conectado",
-    ip: "192.168.0.119",
-    firmware: "V6.14",
-    modelo: "ControlFood",
-    fabricante: "Vitor Costa",
-    equipamento: "Controlador Comedouro",
-    local: "Quartos - PET",
-    porta: "4094",
-    ultima_manutencao: "2024-02-23T14:21:42.000-03:00"
-  },
-  {
-    status: "Desconectado",
-    ip: "192.168.0.219",
-    firmware: "V1.14",
-    modelo: "ControlLight",
-    fabricante: "Vitor Costa",
-    equipamento: "Controlador Luminária",
-    local: "Corredor - PET",
-    porta: "4095",
-    ultima_manutencao: "2024-02-23T14:21:42.000-03:00"
-  },
-  {
-    status: "Conectado",
-    ip: "192.168.0.121",
-    firmware: "V6.14",
-    modelo: "ControlFood",
-    fabricante: "Vitor Costa",
-    equipamento: "Controlador Bebedouro",
-    local: "Quartos - PET",
-    porta: "4096",
-    ultima_manutencao: "2024-02-23T14:21:42.000-03:00"
-  },
-  {
-    status: "Conectado",
-    ip: "192.168.0.152",
-    firmware: "V6.14.10",
-    modelo: "ControlID - IDFace",
-    fabricante: "ControlID",
-    equipamento: "Leitor Facial",
-    local: "Casa 3",
-    porta: "4097",
-    ultima_manutencao: "2024-02-23T14:21:42.000-03:00"
-  },
-  {
-    status: "Conectado",
-    ip: "192.168.0.129",
-    firmware: "V6.14.10",
-    modelo: "ControlID - IDFace",
-    fabricante: "ControlID",
-    equipamento: "Leitor Facial",
-    local: "Casa 2",
-    porta: "4098",
-    ultima_manutencao: "2024-02-23T14:21:42.000-03:00"
-  },
-  {
-    status: "Desconectado",
-    ip: "192.168.0.132",
-    firmware: "V6.14.10",
-    modelo: "ControlID - IDFace",
-    fabricante: "ControlID",
-    equipamento: "Leitor Facial",
-    local: "Casa 1",
-    porta: "4090",
-    ultima_manutencao: "2024-02-23T14:21:42.000-03:00"
-  },
-  {
-    status: "Conectado",
-    ip: "192.168.0.189",
-    firmware: "V6.14.10",
-    modelo: "ControlID - IDFace",
-    fabricante: "ControlID",
-    equipamento: "Leitor Facial",
-    local: "Portão Clausura Canil",
-    porta: "4091",
-    ultima_manutencao: "2024-02-23T14:21:42.000-03:00"
-  },
-  {
-    status: "Conectado",
-    ip: "192.168.0.109",
-    firmware: "V5.18.10",
-    modelo: "BioProx",
-    fabricante: "ControlID",
-    equipamento: "Leitor Biometrico",
-    local: "Portão Clausura",
-    porta: "4099",
-    ultima_manutencao: "2024-02-23T14:21:42.000-03:00"
-  }
-];
+import { Automacao_DATA } from 'src/Database/Automacao';
 
 @Component({
   selector: 'app-automacao',
@@ -198,5 +74,5 @@ export class AutomacaoComponent {
     'acoes'
   ];
 
-  dataSource = ELEMENT_DATA;
+  dataSource = Automacao_DATA;
 }
---
+++ src/app/modules/model/Automacao.ts
@@ -0,0 +1,11 @@
+export interface Automacao {
+    status: string;
+    ip: string;
+    firmware: string;
+    modelo: string;
+    fabricante: string;
+    equipamento: string;
+    local: string;
+    porta: string;
+    ultima_manutencao: string;
+  }
