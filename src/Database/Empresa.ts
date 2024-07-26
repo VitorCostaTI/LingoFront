4af8cb9e41311ba0cb3e124046b7d77d86888466
VitorCostaTI <vitortecnologia62@gmail.com>
2024-07-26 14:06:56 -0300

    Empresa_DATA

---
+++ src/Database/Empresa.ts
@@ -0,0 +1,74 @@
+import { Empresa } from "src/app/modules/model/Empresa";
+
+export const Empresa_DATA: Empresa[] = [
+    {
+        razao_social: 'Kennel LTDA',
+        nome_fantasia: 'Kennel',
+        cnpj: '68.308.719/0001-28',
+        representante: 'Bruno',
+        contato: '(95) 99100-4465'
+    },
+    {
+        razao_social: 'Embrapa ME',
+        nome_fantasia: 'Embrapa',
+        cnpj: '16.985.402/0001-22',
+        representante: 'Bento',
+        contato: 'bento.luan.damata@embrapa.com.br'
+    },
+    {
+        razao_social: 'Lingol Dogs Social',
+        nome_fantasia: 'Lingol Dogs',
+        cnpj: '90.722.864/0001-00',
+        representante: 'Ayla',
+        contato: 'ayla_sophia_daconceicao@lingodogs.com.br'
+    },
+    {
+        razao_social: 'Bento e Isabelle Pets ME',
+        nome_fantasia: 'BentoBelle',
+        cnpj: '26.853.348/0001-15',
+        representante: 'Mariana',
+        contato: '(83) 3724-0515'
+    },
+    {
+        razao_social: 'Pets & Cia ME',
+        nome_fantasia: 'Pets & Cia',
+        cnpj: '70.267.003/0001-71',
+        representante: 'Isis',
+        contato: '(68) 98772-9659'
+    },
+    {
+        razao_social: 'Social Cats LTDA',
+        nome_fantasia: 'Social Cats',
+        cnpj: '25.821.968/0001-00',
+        representante: 'Stella',
+        contato: 'stella_aline_cavalcanti@suplementototal.com.br'
+    },
+    {
+        razao_social: 'Fish Company Social',
+        nome_fantasia: 'Fish Company',
+        cnpj: '14.900.367/0001-76',
+        representante: 'Breno',
+        contato: '(51) 2665-8632'
+    },
+    {
+        razao_social: 'RFDogs Social',
+        nome_fantasia: 'RFDogs',
+        cnpj: '529.269.829.828',
+        representante: 'Ryan',
+        contato: '(93) 99978-4313'
+    },
+    {
+        razao_social: 'SoftHouse ME',
+        nome_fantasia: 'SoftHouse',
+        cnpj: '85.186.922/0001-98',
+        representante: 'Valentina',
+        contato: 'valentina-ramos88@softhouse.net.br'
+    },
+    {
+        razao_social: 'VetePet LTDA',
+        nome_fantasia: 'VetePet',
+        cnpj: '28.321.614/0001-94',
+        representante: 'Daniel',
+        contato: '(86) 99634-7814'
+    },
+];
--- src/app/components/pages/empresas/empresas.component.ts
+++ src/app/components/pages/empresas/empresas.component.ts
@@ -1,90 +1,10 @@
-import { Component }               from '@angular/core';
-import { MatDialog }               from '@angular/material/dialog';
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { EmpresasdialogComponent } from './empresas-dialog/empresas-dialog.component';
 import { ConfirmacaoComponent } from '../../dialogs/confirmacao/confirmacao.component';
 import { ConfirmacaoService } from 'src/app/modules/services/Confirmacao/confirmacao.service';
 import { EmpresasUpdateDialogComponent } from './empresas-update-dialog/empresas-update-dialog.component';
-
-export interface EmpresasElement {
-  razao_social: string;
-  nome_fantasia: string;
-  cnpj: string;
-  representante: string;
-  contato: string;
-}
-
-const ELEMENT_DATA: EmpresasElement[] = [
-  {
-    razao_social: 'Kennel LTDA', 
-    nome_fantasia: 'Kennel', 
-    cnpj: '68.308.719/0001-28', 
-    representante: 'Bruno', 
-    contato: '(95) 99100-4465'
-  },
-  {
-    razao_social: 'Embrapa ME', 
-    nome_fantasia: 'Embrapa', 
-    cnpj: '16.985.402/0001-22', 
-    representante: 'Bento', 
-    contato: 'bento.luan.damata@embrapa.com.br'
-  },
-  {
-    razao_social: 'Lingol Dogs Social', 
-    nome_fantasia: 'Lingol Dogs', 
-    cnpj: '90.722.864/0001-00', 
-    representante: 'Ayla', 
-    contato: 'ayla_sophia_daconceicao@lingodogs.com.br'
-  },
-  {
-    razao_social: 'Bento e Isabelle Pets ME', 
-    nome_fantasia: 'BentoBelle', 
-    cnpj: '26.853.348/0001-15', 
-    representante: 'Mariana', 
-    contato: '(83) 3724-0515'
-  },
-  {
-    razao_social: 'Pets & Cia ME', 
-    nome_fantasia: 'Pets & Cia', 
-    cnpj: '70.267.003/0001-71', 
-    representante: 'Isis', 
-    contato: '(68) 98772-9659'
-  },
-  {
-    razao_social: 'Social Cats LTDA', 
-    nome_fantasia: 'Social Cats', 
-    cnpj: '25.821.968/0001-00', 
-    representante: 'Stella', 
-    contato: 'stella_aline_cavalcanti@suplementototal.com.br'
-  },
-  {
-    razao_social: 'Fish Company Social', 
-    nome_fantasia: 'Fish Company', 
-    cnpj: '14.900.367/0001-76', 
-    representante: 'Breno', 
-    contato: '(51) 2665-8632'
-  },
-  {
-    razao_social: 'RFDogs Social', 
-    nome_fantasia: 'RFDogs', 
-    cnpj: '529.269.829.828', 
-    representante: 'Ryan', 
-    contato: '(93) 99978-4313'
-  },
-  {
-    razao_social: 'SoftHouse ME', 
-    nome_fantasia: 'SoftHouse', 
-    cnpj: '85.186.922/0001-98', 
-    representante: 'Valentina', 
-    contato: 'valentina-ramos88@softhouse.net.br'
-  },
-  {
-    razao_social: 'VetePet LTDA', 
-    nome_fantasia: 'VetePet', 
-    cnpj: '28.321.614/0001-94', 
-    representante: 'Daniel', 
-    contato: '(86) 99634-7814'
-  },
-];
+import { Empresa_DATA } from 'src/Database/Empresa';
 
 @Component({
   selector: 'app-empresas',
@@ -93,8 +13,8 @@ const ELEMENT_DATA: EmpresasElement[] = [
 })
 
 export class EmpresasComponent {
-  
-  constructor(public dialog: MatDialog, private confirmacaoService: ConfirmacaoService ) {}
+
+  constructor(public dialog: MatDialog, private confirmacaoService: ConfirmacaoService) { }
 
   isActive = false;
 
@@ -128,11 +48,11 @@ export class EmpresasComponent {
   displayedColumns: string[] = [
     'razao_social',
     'nome_fantasia',
-    'cnpj', 
+    'cnpj',
     'representante',
     'contato',
     'acoes'
   ];
 
-  dataSource = ELEMENT_DATA;
+  dataSource = Empresa_DATA;
 }
