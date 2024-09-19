5a17badbebaa2f3d86b495767a5a1648f03b33c8
VitorCostaTI <vitortecnologia62@gmail.com>
2024-09-19 10:59:01 -0300

    Atualiza database Usuarios

--- src/app/modules/model/Colaborador.ts
+++ src/app/modules/model/Colaborador.ts
@@ -1,9 +1,17 @@
 export interface Colaborador {
     colaborador: string;
+    documento: string;
+    pis: string;
     setor: string;
-    localizacao: string;
     email: string;
+    password: string;
+    cep: string;
+    logradouro: string;
+    bairro: string;
+    cidade: string;
+    estado: string;
     telefone: string;
     telefone2: string;
+    complemento: string;
     emergencia: string;
 }
