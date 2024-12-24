b32a5aa01ccb489cf577727f65c0c41b4b04e93f
VitorCostaTI <vitortecnologia62@gmail.com>
2024-12-23 22:28:30 -0300

    Automacao: app-automacao-comandos

--- src/app/components/pages/automacao/automacao-comandos/automacao-comandos.component.ts
+++ src/app/components/pages/automacao/automacao-comandos/automacao-comandos.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AutomacoesControlTimeComponent } from '../automacoes-control-time/automacoes-control-time.component';
+import { AutomacoesMonitoramentoComponent } from '../automacoes-monitoramento/automacoes-monitoramento.component';
 
 @Component({
   selector: 'app-automacao-comandos',
   templateUrl: './automacao-comandos.component.html',
   styleUrls: ['./automacao-comandos.component.scss']
 })
+
 export class AutomacaoComandosComponent {
 
+  power1: boolean = true;
+  power2: boolean = true;
+  power3: boolean = true;
+
+  constructor(
+    private dialog: MatDialog,
+  ) { }
+
+  powerFunction1(): void {
+    this.power1 = !this.power1;
+  }
+
+  powerFunction2(): void {
+    this.power2 = !this.power2;
+  }
+
+  powerFunction3(): void {
+    this.power3 = !this.power3;
+  }
+
+  openDialogMonitoramento(enterAnimationDuration: string, exitAnimationDuration: string): void {
+    this.dialog.open(AutomacoesMonitoramentoComponent, {
+      width: '80%',
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+  }
+
+  openDialogTemporizador(enterAnimationDuration: string, exitAnimationDuration: string): void {
+    this.dialog.open(AutomacoesControlTimeComponent, {
+      width: '80%',
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+  }
 }
--- src/app/components/pages/automacao/automacao.component.ts
+++ src/app/components/pages/automacao/automacao.component.ts
@@ -2,24 +2,19 @@ import { Component, HostListener } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { TemplateCrudService } from 'src/app/resources/services/Template/template-crud.service';
 import { AutomacoesDialogComponent } from './automacoes-dialog/automacoes-dialog.component';
-import { AutomacoesControlTimeComponent } from './automacoes-control-time/automacoes-control-time.component';
-import { AutomacoesMonitoramentoComponent } from './automacoes-monitoramento/automacoes-monitoramento.component';
 
 @Component({
   selector: 'app-automacao',
   templateUrl: './automacao.component.html',
   styleUrls: ['./automacao.component.scss']
 })
+
 export class AutomacaoComponent {
 
   quantidade = 25;
+  power: boolean = true;
   isTemperatura: boolean = false;
   isLoading: boolean = false;
-
-  power: boolean = true;
-  power1: boolean = true;
-  power2: boolean = true;
-  power3: boolean = true;
 
   constructor(
     private dialog: MatDialog,
@@ -39,55 +34,11 @@ export class AutomacaoComponent {
   }
 
   powerFunction(): void {
-    if (this.power === true) {
-      this.power = false;
-    } else {
-      this.power = true;
-    }
-  }
-
-  powerFunction1(): void {
-    if (this.power1 === true) {
-      this.power1 = false;
-    } else {
-      this.power1 = true;
-    }
-  }
-
-  powerFunction2(): void {
-    if (this.power2 === true) {
-      this.power2 = false;
-    } else {
-      this.power2 = true;
-    }
-  }
-
-  powerFunction3(): void {
-    if (this.power3 === true) {
-      this.power3 = false;
-    } else {
-      this.power3 = true;
-    }
+    this.power = !this.power;
   }
 
   openDialogAutomacao(enterAnimationDuration: string, exitAnimationDuration: string): void {
     this.dialog.open(AutomacoesDialogComponent, {
-      width: '80%',
-      enterAnimationDuration,
-      exitAnimationDuration,
-    });
-  }
-
-  openDialogTemporizador(enterAnimationDuration: string, exitAnimationDuration: string): void {
-    this.dialog.open(AutomacoesControlTimeComponent, {
-      width: '80%',
-      enterAnimationDuration,
-      exitAnimationDuration,
-    });
-  }
-
-  openDialogMonitoramento(enterAnimationDuration: string, exitAnimationDuration: string): void {
-    this.dialog.open(AutomacoesMonitoramentoComponent, {
       width: '80%',
       enterAnimationDuration,
       exitAnimationDuration,
