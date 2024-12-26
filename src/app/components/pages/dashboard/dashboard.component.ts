8098c9c39304eed8209db4dafd2ebf7faf3049ea
VitorCostaTI <vitortecnologia62@gmail.com>
2024-12-25 23:22:50 -0300

    Dashboard: Sort tabela de agendamentos

--- src/app/components/pages/dashboard/dashboard.component.ts
+++ src/app/components/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Component, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { DespesasComponent } from './dialog/despesas/despesas.component';
 import { FaturamentoComponent } from './dialog/faturamento/faturamento.component';
 import { NascimentoComponent } from './dialog/nascimento/nascimento.component';
 import { RankingComponent } from './dialog/ranking/ranking.component';
 import { Agendamento_DATA } from 'src/database/Agendamento';
+import { MatTableDataSource } from '@angular/material/table';
+import { Agendamento } from 'src/app/resources/model/Agendamento';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-dashboard',
@@ -13,9 +16,16 @@ import { Agendamento_DATA } from 'src/database/Agendamento';
 })
 
 export class DashboardComponent {
+  
+  isActive = false;
+  dataSource = new MatTableDataSource<Agendamento>(Agendamento_DATA);
+
   constructor(public dialog: MatDialog) { }
 
-  isActive = false;
+  @ViewChild(MatSort) sort!: MatSort
+  ngAfterViewInit(): void{
+    this.dataSource.sort = this.sort
+  }
 
   openDialogFaturamento(enterAnimationDuration: string, exitAnimationDuration: string): void {
     this.dialog.open(FaturamentoComponent, {
@@ -56,6 +66,4 @@ export class DashboardComponent {
     'servico',
     'telefone'
   ];
-
-  dataSource = Agendamento_DATA;
 }
